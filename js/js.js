b45f36c98a5b906bfad02625b1c2c1c541977d54
Mudroff <mudrsemen@ya.ru>
2022-07-01 17:03:30 +0300

    menu

--- js/js.js
+++ js/js.js
@@ -7,9 +7,24 @@ $(document).ready(function () {
     items: 3,
     responsive: {
       1200: {
-        loop: true,
         autoWidth: false,
+        items: 3,
+      },
+      698: {
+        items: 2,
+      },
+      320: {
+        items: 1,
       },
     },
   });
+  function openMenu() {
+    let burger = document.querySelector('.header-menu-button');
+    burger.addEventListener('click', function () {
+      let menu = document.querySelector('.header-menu-mobile-list');
+      menu.classList.toggle('header-menu-mobile-list-active');
+      burger.classList.toggle('header-menu-button-close');
+    });
+  }
+  openMenu();
 });
